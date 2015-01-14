6157a6fcfaa259e8c141da27b9ad2b6dd2a1dcb5
Robert <lum.robert@gmail.com>
2015-01-13 20:21:29 -0500

    controlling led using potentiometer values

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 var five = require("johnny-five"),
     // or "./lib/johnny-five" when running from the source
     board = new five.Board(),
-    button;
+    button,
+    potentiometer;
 
 
 board.on("ready", function() {
   var led = new five.Led(3)
-  var button = new five.Button(8);
+  // var button = new five.Button(8);
+  var potentiometer = new five.Sensor({
+  	pin: "A3",
+  	freq: 250
+  });
 
   // led.blink();
 
   this.repl.inject({
   	led: led,
-  	button: button
+  	// button: button,
+  	pot: potentiometer
   });
 
-  button.on('up', function(){
-  	led.off();
-  });
+ //  button.on('up', function(){
+ //  	led.off();
+ //  });
 
-  button.on('hold', function(){
-	led.on();
-  });
+ //  button.on('hold', function(){
+	// led.on();
+ //  });
+
+	potentiometer.on('data', function(){
+		console.log(this.value, this.raw);
+		led.brightness(this.value);
+	});
 
 });
